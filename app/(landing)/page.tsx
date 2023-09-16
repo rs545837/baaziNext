3a08c3272c10f57aa0332853a017809ee2dc0156
rs545837 <rs545837@gmail.com>
2023-09-17 00:29:16 +0530

    Update page.tsx

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,14 +1,18 @@
 import { LandingNavbar } from "@/components/landing-navbar";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingContent } from "@/components/landing-content";
+import { SiteFooter } from "@/components/site-footer";
 
 const LandingPage = () => {
   return ( 
-    <div className="h-full ">
+
+    <div className="h-full">
       <LandingNavbar />
       <LandingHero />
       <LandingContent />
     </div>
+
+    
    );
 }
  
