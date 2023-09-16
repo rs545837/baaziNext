a1e8d70b813d238edaff3a2088670b85d5e1cc7d
rs545837 <rs545837@gmail.com>
2023-09-17 00:20:42 +0530

    contact-us page

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -4,7 +4,7 @@ import {AccordionDemo} from '../faq/accordion'
 const page = () => {
   return (
     <>
-    <h1 className="m-4 text-5xl font-bold">FAQ`&apos;`s</h1>
+    <h1 className="m-4 text-5xl font-bold">FAQ&apos;s</h1>
     <div className='text-center;'>
       <AccordionDemo />
     </div>
